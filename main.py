a964bc2d3fee3ff9c76fcf1423680462ba516c7b
DURUII <wust.durui@qq.com>
2023-06-06 14:34:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tsplib_parser.tsp_file_parser import TSPParser
+from rich import print
 
 if __name__ == '__main__':
-    file_name = "tsplib_benchmark/a280.tsp"
-    TSPParser(filename=file_name, plot_tsp=True)
+    # EUC_2D: 用勾股定理算出两点（城市）间距离后，四舍五入取整
+    with open("tsplib_benchmark/euc_2d", "r") as fin:
+        names = [line.strip() for line in fin.readlines()]
+        for name in names:
+            TSPParser(name)
+    # print(TSPParser.tsp_cities_dict)
