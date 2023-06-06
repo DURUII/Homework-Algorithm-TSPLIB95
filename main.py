2f30e74bc72788dab67bd1c233ca4afe4c6b93c0
DURUII <wust.durui@qq.com>
2023-06-06 23:05:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import tsplib_algorithm.greedy_heuristic as gh
 from tsplib_parser.tsp_file_parser import TSPParser
 from rich import print
 
 if __name__ == '__main__':
-    # EUC_2D: 用勾股定理算出两点（城市）间距离后，四舍五入取整
     with open("tsplib_benchmark/euc_2d", "r") as fin:
         names = [line.strip() for line in fin.readlines()]
-        for name in names:
-            TSPParser(name)
-    # print(TSPParser.tsp_cities_dict)
+
+    # a280
+    TSPParser(names[0], plot_tsp=True)
+    gh.city_nearest_neighbor()
+    TSPParser.plot()
