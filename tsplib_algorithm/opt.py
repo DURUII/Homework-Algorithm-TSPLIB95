35e62b564ca03417fd9ce4a9df6d973fb61f11bb
DURUII <wust.durui@qq.com>
2023-06-10 01:36:18 +0800

    Update opt.py

--- tsplib_algorithm/opt.py
+++ tsplib_algorithm/opt.py
@@ -1,17 +1,15 @@
 from typing import List
 
-import networkx as nx
-
-from tsplib_utils.helper import length_of_a_tour
+from tsplib_utils.parser import TSPParser
 
 
-def two_opt(G: nx.Graph, permutation: List[int]) -> (List[int], int):
+def two_opt(permutation: List[int]) -> (List[int], int):
     """
     ref:
     https://stackoverflow.com/questions/53275314/2-opt-algorithm-to-solve-the-travelling-salesman-problem-in-python
     """
     tour = permutation[:]
-    min_length = length_of_a_tour(G, tour)
+    min_length = TSPParser.length_of_a_tour(tour)
 
     improved = True
     while improved:
@@ -20,7 +18,7 @@ def two_opt(G: nx.Graph, permutation: List[int]) -> (List[int], int):
             for j in range(i + 2, len(tour)):
                 temp = tour[:]
                 temp[i + 1:j + 1] = temp[j:i:-1]
-                length = length_of_a_tour(G, temp)
+                length = TSPParser.length_of_a_tour(temp)
                 if length < min_length:
                     min_length = length
                     improved = True
