571b51c4b762a3cf417f86d86d3631f12328b940
DURUII <wust.durui@qq.com>
2023-11-07 11:25:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from tsplib_algorithm.nn import GreedyNearestNeighbor
 from tsplib_algorithm.sa import SimulatedAnnealing
 from tsplib_algorithm.wl import WangLeiAlgorithm
 from tsplib_algorithm.ga import GeneticAlgorithm
+from tsplib_algorithm.pso import ParticleSwarmOpt
 from tsplib_problem.base import Problem
 from tsplib_algorithm.opt import Opt2
 
-problem = Problem('a280', verbose=True, vis=False)
+problem = Problem('a280', verbose=False, vis=False)
+solver = Opt2(base_solver=ChristofidesSerdyukov())
+solver.solve(problem)
+
 solver = GeneticAlgorithm()
 solver.solve(problem)
-print(problem.best_seen.length)
