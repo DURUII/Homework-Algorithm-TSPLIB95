462c4111cfedf9d34136cac21b630538f4623fe2
DURUII <wust.durui@qq.com>
2023-11-07 11:25:18 +0800

    Update ga.py

--- tsplib_algorithm/ga.py
+++ tsplib_algorithm/ga.py
@@ -1,3 +1,4 @@
+import math
 import random
 import time
 
@@ -12,11 +13,9 @@ from tsplib_problem.base import Problem
 
 class GeneticAlgorithm(Algorithm):
     def __init__(self, tag='GeneticAlgorithm', verbose=True, boost=False,
-                 time_out=1200, size=1120, pc=0.9, pm=0.3):
+                 time_out=600, size=300, pc=0.9, pm=0.4):
         super().__init__(tag, verbose, boost)
-        self.operator = [naive_swap, naive_insert, naive_reverse,
-                         opt_swap_2, opt_swap_2, opt_swap_2,
-                         opt_swap_3, opt_swap_3, opt_swap_3]
+        self.operator = [naive_insert, naive_reverse, naive_swap, opt_swap_2, opt_swap_3]
 
         # time of mimicking evolution
         self.time_out = time_out
@@ -34,18 +33,25 @@ class GeneticAlgorithm(Algorithm):
         # (genotype equals phenotype in this special case)
         population = []
 
-        # init individuals: generate random population
-        for _ in range(self.size):
-            # a list of n integers, each of which occurs exactly once
-            # n.b. an individual exactly represents a tour
-            population.append(list(np.random.permutation([i for i in range(1, problem.dimension + 1)])))
+        if problem.best_seen.tour is not None:
+            for _ in range(self.size):
+                population.append(
+                    random.choice(self.operator)(problem.best_seen.tour)
+                )
+        else:
+            # init individuals: generate random population
+            for _ in range(self.size):
+                # a list of n integers, each of which occurs exactly once
+                # n.b. an individual exactly represents a tour
+                population.append(list(np.random.permutation([i for i in range(1, problem.dimension + 1)])))
 
         # before it is too late
         tic = time.perf_counter()
         while time.perf_counter() - tic < self.time_out:
             epoch += 1
             # selection (survival of the fittest)
-            mating_pool = self.select_rank(population, problem)
+            mating_pool = self.select_diversity(population, problem)
+            print(f'{epoch} - selected with best seen {problem.best_seen.length}')
             # crossover/inheritance
             offspring = self.crossover(mating_pool)
             # in-place mutate/variation
@@ -65,7 +71,9 @@ class GeneticAlgorithm(Algorithm):
         for i in range(1, len(roulette)):
             roulette[i] += roulette[i - 1]
 
-        for _ in range(len(population)):
+        # inheritance strategy
+        selected.append(problem.best_seen.tour)
+        for _ in range(len(population) - 1):
             target = random.random()
             lo, hi = 0, len(roulette) - 1
             while lo < hi:
@@ -79,7 +87,7 @@ class GeneticAlgorithm(Algorithm):
         return selected
 
     def select_rank(self, population: list[list[int]], problem: Problem):
-        C = 0.35
+        C = 0.3
         selected = []
         sorted_population = sorted(population, key=lambda p: problem.calculate_length(p, leaderboard=True))
 
@@ -93,8 +101,32 @@ class GeneticAlgorithm(Algorithm):
         return selected
 
     def select_diversity(self, population: list[list[int]], problem: Problem):
-        # FIXME 39:09
-        pass
+        length = [problem.calculate_length(i, leaderboard=True) for i in population]
+        diversity = []
+        for i in range(len(length)):
+            diversity.append(0)
+            for j in range(i + 1, len(length)):
+                diversity[i] -= abs(length[j] - length[i])
+
+        C = 0.35
+        selected = []
+        order_length = np.argsort(length)
+        order_diversity = np.argsort(diversity)
+
+        # inheritance strategy
+        selected.append(problem.best_seen.tour)
+        for _ in range(len(population) - 1):
+            idx, target = 0, 1
+            while random.random() < target * (1 - C):
+                idx += 1
+                target *= C
+
+            if random.random() < 0.75:
+                selected.append(population[order_length[idx]])
+            else:
+                selected.append(population[order_diversity[idx]])
+
+        return selected
 
     def crossover(self, population: list[list[int]]):
         offspring = []
